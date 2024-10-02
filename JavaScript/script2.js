b49dedb94dd0714574d4fec159a4aef35b34c877
Khaldoun Halawani <khalawani@sadasol.com>
2024-10-02 19:49:37 +0300

    JS3 Callback functions

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -11,7 +11,7 @@ function calc(n1, n2) {
 }
 
 // es6 (arrow function)
-const calc2 = (n1, n2) => {
+const calc2 = (n1, n2 = 5) => {
     const res = (n1 * n1) + n2;
     return res;
 }
@@ -27,12 +27,25 @@ const calc3 = (n1, n2) => (n1 * n1) + n2;
 // }
 
 // shorter form
-const isPass = avg => avg >= 60 ? true : false;
+const isPass = avg => avg >= 50 ? true : false;
 const isPassVS = avg => avg >= 60;
 
 console.log(calc2(8, 6));
+console.log("One param (9):" + calc2(9));
 console.log(calc3(5, 2));
 
 console.log(isPassVS(50));
 console.log(isPassVS(60));
 console.log(isPass(70));
+
+const calcAvg = (m1, m2, m3, isPassFunc) => {
+    const avg = (m1 + m2 + m3) / 3;
+    const is_pass = isPassFunc(avg);
+    console.log("Average is: " + avg + " and did the student pass: " + is_pass);
+};
+
+// University student
+calcAvg(90, 70, 60, isPassVS);
+
+// School student
+calcAvg(65, 50, 57, isPass);
